92c127a94199ec10049bd167e4e60c0615c86b1f
yusufduyar <yusufduyar@outlook.com>
2017-12-08 15:52:54 +0300

    Refactoring

--- src/main/java/com/spring5/recipeapp/services/RecipeService.java
+++ src/main/java/com/spring5/recipeapp/services/RecipeService.java
@@ -2,11 +2,13 @@ package com.spring5.recipeapp.services;
 
 import com.spring5.recipeapp.domain.Recipe;
 import com.spring5.recipeapp.repositories.IRecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeService implements IRecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeService implements IRecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("This is from service");
         Set<Recipe> recipes =new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
 
